c76d17abb86097fb61de4041639112a2e7f5b75e
VaibhavKukreti-Kathud <9720440160beena@gmail.com>
2023-07-12 12:26:41 +0530

    Initial commit

---
+++ pages/auth.tsx
@@ -0,0 +1,17 @@
+const Auth = () => {
+    return (
+        <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover bg-fixed">
+            <div className="bg-black w-full h-full bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <img src="/images/logo.png" alt="logo" className="h-12"></img>
+                </nav>
+                <div className="flex justify-center">  
+                <div className="">  </div> 
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Auth;
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <h1 className="text-2xl text-blue-400">
+      Netflix Clone
+    </h1>
+  )
+}
